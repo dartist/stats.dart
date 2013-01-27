fd36cb2e21238655cf819fad7994ecba73e64452
adam <financeCoding@gmail.com>
2013-01-26 21:57:58 -0800

    Including runner and analysis script

---
+++ test/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+#####
+# Unit Tests
+
+echo "DumpRenderTree test/tests_browser.html"
+results=`DumpRenderTree test/tests_browser.html 2>&1`
+
+echo "$results" | grep CONSOLE
+
+echo $results | grep 'unittest-suite-success' >/dev/null
+
+echo $results | grep -v 'Exception: Some tests failed.' >/dev/null
+
+#####
+# Type Analysis
+
+echo
+echo "dart_analyzer lib/*.dart"
+
+results=`dart_analyzer lib/*.dart 2>&1`
+
+echo "$results"
+
+if [ -n "$results" ]; then
+    exit 1
+else
+    echo "Passed analysis."
+fi
